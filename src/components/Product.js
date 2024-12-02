e4c2eac1a2bdea06d895f0ac5f7d2eb1c5751768
Mahdi Karimi <mhdksp566@gmail.com>
2024-12-02 23:35:22 +0330

    completed

--- src/components/Product.js
+++ src/components/Product.js
@@ -1,8 +1,12 @@
+import { Link } from "react-router-dom";
+
 const Product = ({ item }) => {
     return (
         <div className="col-lg-3">
             <div class="card">
+                <Link to={`/products/${item.id}`}>
                 <img src={item.images[0]} class="card-img-top" alt={item.title} />
+                </Link>
                 <div class="card-body">
                     <h5 class="card-title">{item.title}</h5>
                     <p class="card-text">{item.description}</p>
--- src/pages/DetailProduct.js
+++ src/pages/DetailProduct.js
@@ -1,13 +1,41 @@
-import Navbar from "../components/Navbar";
-
-
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
 
 
 const DetailProduct = () => {
-    
+    const { id } = useParams();
+    const [data, setData] = useState(null);
+
+    useEffect(() => {
+        fetch(`https://api.escuelajs.co/api/v1/products/${id}`).then(res => res.json()).then(item => setData(item))
+    }, [])
 
 
 
+    return (
+        
+        <div className="container">
+            {data && <div className="row">
+                <div className="col-lg-6">
+                    <img src={data.images[0]} className="img-fluid" />
+                </div>
+                <div className="col-lg-6">
+                    <h1>
+                        {data.title}
+                    </h1>
+                    <p>
+                        {data.description}
+                    </p>
+
+
+                </div>
+            </div>}
+        </div>
+
+
+
+
+    )
 }
 
 
